4889c6f59344798bee56d685682c84ce3310b15f
Chris <chris@cabtdesigns.com>
2021-10-12 16:43:04 -0700

    pseudo code for calculator

---
+++ calculator/calculator.php
@@ -0,0 +1,24 @@
+<?php
+
+// Need to build a calculator that takes user input, then takes info from 2 tables in a database to outputs
+// several results. it will give the user lowest available rate, competitive rate, cash out, cost and LTV value.
+
+
+// connect to database
+
+//get results from database tables
+
+//save results from database
+
+// take user input frpom a form
+    // from needs to have FICO, Loan Amount, Value
+
+// decide what data to use from the database tables
+
+// run calculations from user and database using if statments
+
+// display results
+
+
+
+
