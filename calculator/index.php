ee32bc9e1257e0c4c2de6f3951cdc5e8f5724f4e
Chris <chris@cabtdesigns.com>
2021-10-12 15:16:57 -0700

    added view recorod and nav menu

--- calculator/index.php
+++ calculator/index.php
@@ -1,21 +1,26 @@
-<?php
-
-include '../../../mmlt_database/db.php';
-
-// check connection
-    if(!$conn){
-        echo 'Connection error: ' . mysqli_connect_error();
-    }
-
-?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-    </head>
-    <body>
-        <div>
-            hi there buddy
-        </div>
-    </body>
-</html>
+<?php
+
+include '../../../mmlt_database/db.php';
+
+// check connection
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <div>
+            <a href="index.php">Home</a>
+            <a href="view.php">View records / Edit</a>
+            <a href="insert.php">Insert new record</a>
+            <?php include 'view.php'; ?>
+
+        </div>
+    </body>
+</html>
+
