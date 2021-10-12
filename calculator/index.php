c935f8ca2ee88fa29df83dc822482c5ee1c35100
Chris <chris@cabtdesigns.com>
2021-10-12 12:44:07 -0700

    connecting to database password in include file

--- calculator/index.php
+++ calculator/index.php
@@ -1,22 +1,21 @@
-<?php
-
-$conn = mysqli_connect('localhost', 'igpwxwmy_will', '', 'igpwxwmy_mmlt');
-
-// check connection
-    if(!$conn){
-        echo 'Connection error: ' . mysqli_connect_error();
-    }
-
-?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-    </head>
-    <body>
-        <div>
-            hi there buddy
-        </div>
-    </body>
-</html>
-
+<?php
+
+include '../../../mmlt_database/db.php';
+
+// check connection
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <div>
+            hi there buddy
+        </div>
+    </body>
+</html>
