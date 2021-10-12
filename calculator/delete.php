90f8213c59ee7df88da79049a2c3f7dbefd0e512
Chris <chris@cabtdesigns.com>
2021-10-12 15:17:32 -0700

    can edit add and delete from database

---
+++ calculator/delete.php
@@ -0,0 +1,7 @@
+<?php
+include '../../../mmlt_database/db.php';
+$id=$_REQUEST['id'];
+$query = "DELETE FROM homeready_30yr WHERE id=$id"; 
+$result = mysqli_query($conn,$query) or die ( mysqli_error());
+header("Location: view.php"); 
+?>
---
+++ calculator/view.php
@@ -0,0 +1,46 @@
+<?php
+include '../../../mmlt_database/db.php';
+
+// check connection
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+?>
+
+<div class="form">
+<p>
+    <a href="index.php">Home</a>
+    <a href="insert.php">Insert new record</a>
+    <a href="view.php">View records</a>
+<h2>View Records</h2>
+<h3>Home Ready 30 Year</h3>
+<table width="100%" border="1" style="border-collapse:collapse;">
+<thead>
+<tr>
+<th><strong>ID</strong></th>
+<th><strong>Rate</strong></th>
+<th><strong>15 Day</strong></th>
+<th><strong>Edit</strong></th>
+<th><strong>Delete</strong></th>
+</tr>
+</thead>
+<tbody>
+<?php
+$count=1;
+$sel_query="Select * from homeready_30yr ORDER BY id asc;";
+$result = mysqli_query($conn,$sel_query);
+while($row = mysqli_fetch_assoc($result)) { ?>
+<tr><td align="center"><?php echo $row["id"];?></td>
+<td align="center"><?php echo $row["rate"]; ?></td>
+<td align="center"><?php echo $row["fifteenday"]; ?></td>
+<td align="center">
+<a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a>
+</td>
+<td align="center">
+<a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a>
+</td>
+</tr>
+<?php $count++; } ?>
+</tbody>
+</table>
+</div>
