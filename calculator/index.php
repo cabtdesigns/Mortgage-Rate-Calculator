e31a124847078ac865d83a470a41d3c1731abb1a
Chris <chris@cabtdesigns.com>
2021-10-12 12:20:35 -0700

    connected to databases but in public view

---
+++ calculator/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'igpwxwmy_will', '', 'igpwxwmy_mmlt');
+
+// check connection
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <div>
+            hi there buddy
+        </div>
+    </body>
+</html>
+
