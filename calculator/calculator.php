9a3146add12277d302c5fc1756844e71749612e8
Chris <chris@cabtdesigns.com>
2021-10-13 12:30:02 -0700

    adding using user input and database info

--- calculator/calculator.php
+++ calculator/calculator.php
@@ -1,5 +1,4 @@
 <?php
-
 // Need to build a calculator that takes user input, then takes info from 2 tables in a database to outputs
 // several results. it will give the user lowest available rate, competitive rate, cash out, cost and LTV value.
 
@@ -13,22 +12,38 @@ include '../../../mmlt_database/db.php';
         echo 'Connection error: ' . mysqli_connect_error();
     }
 ?>
-<!-- get results from database tables -->
-
-<!-- save results from database -->
 
 <!-- // take user input frpom a form -->
 
-<?php 
+<?php
+
+$userFico = $_REQUEST['userFICO'];
+$userLoanAmount = $_REQUEST['user_loan_amount'];
+$userPropertyValue = $_REQUEST['userValue'];
+
 $status = "";
 if(isset($_POST['new']) && $_POST['new']==1){
-    $userFico = $_REQUEST['userFICO'];
-    $userLoanAmount = $_REQUEST['user_loan_amount'];
-    $userPropertyValue = $_REQUEST['userValue'];
-    $status = 'Fico ' . $userFico . ' ' . 'Loan Amount ' . $userLoanAmount . ' ' . 'Value ' . $userPropertyValue;
+    $userFicoResults = $userFico;
+    $userLoanAmountResults = $userLoanAmount;
+    $userPropertyValueResults = $userPropertyValue;
+    $status = 'Fico ' . $userFicoResults . ' ' . 'Loan Amount ' . $userLoanAmountResults . ' ' . 'Value ' . $userPropertyValueResults;
 }
 ?>
-    <!-- // from needs to have FICO, Loan Amount, Value -->
+
+<!-- save results from database -->
+<?php
+$adding = "";
+if ($userFico == 740){
+    $query = "Select * from homeready_30yr Where id='10'";
+    $results = mysqli_query($conn, $query) or die(mysqli_error());
+    $row = mysqli_fetch_array($results);
+    $rate = $row['rate'];
+    
+    //$ficoadd = $userFico + $userFico;
+    $adding =  $rate + $userFico;
+}
+?>
+    <!-- User input form -->
 <!DOCTYPE html>
 <head>
 </head>
@@ -42,10 +57,28 @@ if(isset($_POST['new']) && $_POST['new']==1){
             <div><p><input name="submit" type="submit" value="Submit" /></p></div>
     </form>
     <p style="color:#FF0000;"><?php echo $status; ?></p>
+    <p style="color:#FF0000;"><?php echo $adding; ?></p>
+
+
+<?php 
+
+?>
+
+
+<?php //$sql = "SELECT rate FROM homeready_30yr WHERE id='01'";
+// execute query and store result
+//$homeReady30 = mysqli_query($conn, $sql);
+//echo $homeReady30;
+?>
 </body>
 </html>
+
+<!-- get specific results from database tables -->
+
+
 <!-- // decide what data to use from the database tables -->
 
 <!-- // run calculations from user and database using if statments -->
 
 <!-- // display results -->
+
